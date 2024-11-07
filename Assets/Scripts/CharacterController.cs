6d38553959acbe3decd5b84e710b754a08921a93
Rabia Nur Özdemir <minozdemir7@gmail.com>
2024-11-07 21:12:10 +0300

    Numeric blocks added.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public GameManager gameManagerScript;
 
     void FixedUpdate() // So movements will be perceived more better
     {
@@ -27,6 +28,14 @@ public class CharacterController : MonoBehaviour
                 transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z), 0.3f); // Move the character right
 
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "x2" || other.name == "+3" || other.name == "-4" || other.name == "/2") // Check if the character hits the numeric blocks
+        {
+            gameManagerScript.CharacterManagement(other.name, other.transform); // Call the CharacterManagement method in the GameManager script }
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject destinationPoint;
-    public GameObject spawnPoint;
     public List<GameObject> subCharacters;
     public int currentCharacterCount = 1; // Including main character
 
@@ -17,19 +17,138 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            foreach (var item in subCharacters)
-            {
-                if (!item.activeInHierarchy)
-                {
-                    item.transform.position = spawnPoint.transform.position; // Set the position of the subCharacter to the spawnPoint
-                    item.SetActive(true);
-                    currentCharacterCount++;
-                    break; // To prevent all subCharacters to be activated
-                }
-            }
-        }
+    }
 
+    public void CharacterManagement(string name, Transform transform)
+    {
+        switch (name)
+        {
+            case "x2":
+                int count = 0;
+                foreach (var item in subCharacters)
+                {
+                    if (count < currentCharacterCount)
+                    {
+                        if (!item.activeInHierarchy)
+                        {
+                            item.transform.position = transform.position + new Vector3(0, 0f, -0.4f); // Set the position of the sub character to the position of the numeric block
+                            item.SetActive(true);
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        count = 0;
+                        break;
+                    }
+
+                }
+                currentCharacterCount *= 2;
+                break;
+
+            case "+3":
+                int count2 = 0;
+                foreach (var item in subCharacters)
+                {
+                    if (count2 < 3)
+                    {
+                        if (!item.activeInHierarchy)
+                        {
+                            item.transform.position = transform.position + new Vector3(0, 0f, -0.4f); // Set the position of the sub character to the position of the numeric block
+                            item.SetActive(true);
+                            count2++;
+                        }
+                    }
+                    else
+                    {
+                        count2 = 0;
+                        break;
+                    }
+                }
+                currentCharacterCount += 3;
+                break;
+
+
+            case "-4":
+                if (currentCharacterCount <= 4)
+                {
+                    foreach (var item in subCharacters)
+                    {
+
+                        item.transform.position = Vector3.zero;
+                        item.SetActive(false);
+                    }
+                    currentCharacterCount = 1;
+                }
+                else
+                {
+                    int count3 = 0;
+                    foreach (var item in subCharacters)
+                    {
+                        if (count3 < 4)
+                        {
+                            if (item.activeInHierarchy)
+                            {
+                                item.transform.position = Vector3.zero;
+                                item.SetActive(false);
+                                count3++;
+                            }
+                        }
+                        else
+                        {
+                            count3 = 0;
+                            break;
+                        }
+                    }
+                    currentCharacterCount -= 4;
+                }
+                break;
+
+            case "/2":
+                if (currentCharacterCount <= 2)
+                {
+                    foreach (var item in subCharacters)
+                    {
+
+                        item.transform.position = Vector3.zero;
+                        item.SetActive(false);
+                    }
+                    currentCharacterCount = 1;
+                }
+                else
+                {
+                    int count3 = 0;
+                    foreach (var item in subCharacters)
+                    {
+                        if (count3 != currentCharacterCount / 2)
+                        {
+                            if (item.activeInHierarchy)
+                            {
+                                item.transform.position = Vector3.zero;
+                                item.SetActive(false);
+                                count3++;
+                            }
+                        }
+                        else
+                        {
+                            count3 = 0;
+                            break;
+                        }
+                    }
+
+                    if (currentCharacterCount % 2 == 0)
+                    {
+                        currentCharacterCount /= 2;
+                    }
+                    else
+                    {
+                        currentCharacterCount = (currentCharacterCount / 2) + 1;
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
